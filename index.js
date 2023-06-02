27d13f97c7b4fe770cfcd6ad6fa5f2d439064d73
Humayun63 <humayun126224@gmail.com>
2023-06-03 01:03:26 +0600

    .

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ async function run() {
         })
         
         app.get('/carts', async(req, res)=>{
-            const email = req.query.email;
+            const email = req.query.email
+            console.log(email)
             const query = {email : email}
             const result = await cartCollection.find(query).toArray()
             res.send(result)
@@ -53,6 +54,11 @@ async function run() {
             const cart = req.body;
             const result = await cartCollection.insertOne(cart)
             res.send(result)
+        })
+
+        app.delete('carts/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query
         })
 
 
