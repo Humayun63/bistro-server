cd3c3197249b7a6f7f1f7497b9de090ad7345826
Humayun63 <humayun126224@gmail.com>
2023-06-02 20:13:52 +0600

    cart api created

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
 
         const menuCollection = client.db('bistroDB').collection('menu')
         const reviewCollection = client.db('bistroDB').collection('reviews')
+        const cartCollection = client.db('bistroDB').collection('carts')
 
 
         app.get('/menu', async(req, res) =>{
@@ -40,7 +41,19 @@ async function run() {
             const result = await reviewCollection.find().toArray()
             res.send(result)
         })
+        
+        app.get('/carts', async(req, res)=>{
+            const email = req.query.email;
+            const query = {email : email}
+            const result = await cartCollection.find(query).toArray()
+            res.send(result)
+        })
 
+        app.post('/carts', async(req, res)=>{
+            const cart = req.body;
+            const result = await cartCollection.insertOne(cart)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
