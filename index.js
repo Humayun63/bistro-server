bec85a23cf342d0928a119a9b646efa04a313779
Humayun63 <humayun126224@gmail.com>
2023-06-03 12:43:42 +0600

    jwt start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -32,40 +33,85 @@ async function run() {
         const cartCollection = client.db('bistroDB').collection('carts')
         const usersCollection = client.db('bistroDB').collection('users')
 
+
+
+        app.post('/jwt', (req, res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_KEY, {expiresIn:'1h'})
+            res.send({token})
+        })
+
         // Users related apis
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+
+            const loggedUser = await usersCollection.findOne(query)
+            console.log(loggedUser);
+            if (loggedUser) {
+                return res.send({ message: 'Already Exits' })
+            }
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        // users admin apis
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
+        app.delete('/users/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await usersCollection.deleteOne(query)
+            res.send(result)
+
+        })
 
         // Menu related apis
-        app.get('/menu', async(req, res) =>{
+        app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
 
 
         // Reviews related apis
-        app.get('/reviews', async(req, res) =>{
+        app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray()
             res.send(result)
         })
-        
+
 
         // Carts related apis
-        app.get('/carts', async(req, res)=>{
+        app.get('/carts', async (req, res) => {
             const email = req.query.email
-            console.log(email)
-            const query = {email : email}
+            const query = { email: email }
             const result = await cartCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.post('/carts', async(req, res)=>{
+        app.post('/carts', async (req, res) => {
             const cart = req.body;
             const result = await cartCollection.insertOne(cart)
             res.send(result)
         })
 
-        app.delete('/carts/:id', async(req, res)=>{
+        app.delete('/carts/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
